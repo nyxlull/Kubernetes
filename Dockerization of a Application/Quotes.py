13385616fc0eb16487a4f517f76e35a66a9ecac4
Micah <micah8844@gmail.com>
2025-04-24 09:53:52 -0400

    Quotes.py

---
+++ Dockerization of a Application/Quotes.py
@@ -0,0 +1,12 @@
+import random
+
+quotes = [
+    "The best way to get started is to quit talking and begin doing.",
+    "Don’t let yesterday take up too much of today.",
+    "It’s not whether you get knocked down, it’s whether you get up.",
+    "If you are working on something exciting, it will keep you motivated.",
+    "Success is not in what you have, but who you are."
+]
+
+def get_random_quote():
+    return random.choice(quotes)
